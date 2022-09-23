41414ea00072e4b0478954d9400be206fc6874a8
erichlof <erichlof@swbell.net>
2022-09-23 14:12:38 -0500

    add chapter 3

---
+++ Chapter_0/Episode_3/js/interactivePage.js
@@ -0,0 +1,59 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", handleWindowResize);
+function handleWindowResize()
+{
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	halfCanvasW = canvas.width / 2;
+	halfCanvasH = canvas.height / 2;
+
+	draw();
+}
+
+let text = document.getElementById("myText");
+window.addEventListener("mousedown", handleMouseDown);
+function handleMouseDown()
+{
+	text.innerHTML = "You pressed the mouse!";
+}
+
+window.addEventListener("mouseup", handleMouseUp);
+function handleMouseUp()
+{
+	text.innerHTML = "This is some text";
+}
+
+let halfCanvasW = canvas.width / 2;
+let halfCanvasH = canvas.height / 2;
+
+let rectW = 40;
+let rectH = 20;
+
+let halfRectW = rectW / 2;
+let halfRectH = rectH / 2;
+
+function draw()
+{
+	context.fillStyle = "magenta";
+	context.fillRect(halfCanvasW - halfRectW, halfCanvasH - halfRectH, rectW, rectH);
+	
+	context.fillStyle = "red";
+	context.fillRect(0, 0, rectW, rectH);
+	
+	context.fillStyle = "white";
+	context.fillRect(canvas.width - rectW, 0, rectW, rectH);
+	
+	context.fillStyle = "blue";
+	context.fillRect(0, canvas.height - rectH, rectW, rectH);
+	
+	context.fillStyle = "green";
+	context.fillRect(canvas.width - rectW, canvas.height - rectH, rectW, rectH);
+}
+
+draw();
+
